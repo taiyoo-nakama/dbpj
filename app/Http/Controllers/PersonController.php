f085c1181434b3e40133e947d0d91d273a77da7d
仲間太陽 <taiyoonakama@gmail.com>
2021-06-30 13:25:06 +0900

    9th-commit

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -9,8 +9,8 @@ use Illuminate\Http\Request;
 {
     public function index(Request $request)
     {
-            $items = DB::table('people')->simplePaginate(5);
-            return view('index',['items'=>$items]);
+        $items = Person::simplePaginate(5);
+        return view('index',['items'=>$items]);
     }
     public function find(Request $request)
     {
